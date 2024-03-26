ab16e7f4a4c27224fa0c1fbd7e9cc058f863220c
md-maheen-billah <md.maheen.billah@icloud.com>
2024-03-27 00:33:04 +0100

    implemented pages to read section

--- src/pages/PagesToRead.jsx
+++ src/pages/PagesToRead.jsx
@@ -1,7 +1,12 @@
+import Chart from "../components/Chart";
+
 const PagesToRead = () => {
   return (
     <div>
       <h2>Pages to Read</h2>
+      <div>
+        <Chart></Chart>
+      </div>
     </div>
   );
 };
